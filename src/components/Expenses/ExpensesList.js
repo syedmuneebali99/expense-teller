9cc5e2a0202fac3d288941289685a985d0886402
syedmuneebali99 <alimuneeb3565@gmail.com>
2022-07-22 17:30:38 +0530

    final commit

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <h3 className="expenses-list__fallback">
+        Found No Expenses In Year :{" "}
+        <span className="filtered-year">{props.filteredYear}</span>
+      </h3>
+    );
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
